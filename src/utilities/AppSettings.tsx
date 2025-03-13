8240c68b98e312b2673882f670808fb74d56cf6a
sivakami <sivakamia017@gmail.com>
2025-03-13 17:30:54 +0530

    Update AppSettings.tsx

--- src/utilities/AppSettings.tsx
+++ src/utilities/AppSettings.tsx
@@ -1,7 +1,7 @@
 
-const baseUrl = "https://crmqaapi.truckergig.com/common/";
-const userUrl = "https://crmqaapi.truckergig.com/users/";
-const clientsUrl = "https://crmqaapi.truckergig.com/clients/";
+const baseUrl = "https://crmapi.truckergig.com/common/";
+const userUrl = "https://crmapi.truckergig.com/users/";
+const clientsUrl = "https://crmapi.truckergig.com/clients/";
 const urls = {
   baseUrl: baseUrl,
   userUrl:userUrl,
